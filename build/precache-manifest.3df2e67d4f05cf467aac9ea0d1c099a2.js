a7cf4d76b805acdcbf8469e4bc964e352686bbac
houser <houserqu@qq.com>
2018-12-21 09:49:58 +0800

    feat:build

---
+++ build/precache-manifest.3df2e67d4f05cf467aac9ea0d1c099a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "fad37861c8a434db82aa",
+    "url": "./static/js/main.fad37861.chunk.js"
+  },
+  {
+    "revision": "218dd6a8b2914d82c0a8",
+    "url": "./static/js/1.218dd6a8.chunk.js"
+  },
+  {
+    "revision": "fad37861c8a434db82aa",
+    "url": "./static/css/main.10de9cad.chunk.css"
+  },
+  {
+    "revision": "218dd6a8b2914d82c0a8",
+    "url": "./static/css/1.b97364b2.chunk.css"
+  },
+  {
+    "revision": "7a6906f52780ac47e5e907ddb52122c1",
+    "url": "./index.html"
+  }
+];
